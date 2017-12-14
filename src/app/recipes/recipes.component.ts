5af5697de34ce6c572688e4b9ecd81ebe15f5af3
Abhishek Basak <Abhishek.Basak@Pearson.com>
2017-12-14 13:34:41 -0600

    Print recipe lists

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Tandoori Chicken', 'Tandoori Chicken',
+                'https://www.yummytummyaarthi.com/wp-content/uploads/2014/10/1-34.jpg'),
+    new Recipe('Mutton Biriyani', 'Mutton Biriyani',
+                'https://i0.wp.com/www.foodvedam.com/wp-content/uploads/2014/11/Mutton-Biryani2.jpg?resize=650%2C488')
+  ];
 
   constructor() { }
 
