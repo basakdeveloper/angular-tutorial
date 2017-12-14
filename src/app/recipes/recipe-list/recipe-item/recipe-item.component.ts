969687f7bb986ef391a003521eeee969b14225d7
Abhishek Basak <Abhishek.Basak@Pearson.com>
2017-12-14 14:44:54 -0600

    Model folder

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,5 @@
+import { Recipe } from './../../../models/recipe.model';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Recipe } from './../recipe.model';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
