11c29ba8145835b2e3695536d2af3e9396c6e2bf
Abhishek Basak <Abhishek.Basak@Pearson.com>
2017-12-13 22:36:53 -0600

    Component base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './header/recipes/recipes.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './header/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './header/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './header/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './header/recipes/recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
